35ceb94291029241c4de2755138f804a248ce4f4
Cory Powell <coryrpowell@gmail.com>
2023-07-07 18:04:47 -0400

    Create data and next node

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,4 +2,16 @@ require "rspec"
 require "./lib/node"
 
 describe Node do
+    it "is an instance of node" do
+        node = Node.new("plop")
+
+        expect(node).to be_an_instance_of(Node)
+        expect(node.data).to eq("plop")
+    end
+
+    it "creates the next node" do
+        node = Node.new("plop")
+
+        expect(node.next_node).to eq(nil)
+    end
 end
