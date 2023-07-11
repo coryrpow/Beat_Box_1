16dff71f1650bfe4a96c2472425e317e953d721d
Cory Powell <coryrpowell@gmail.com>
2023-07-10 21:31:31 -0400

    Add next node

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -6,7 +6,6 @@ describe LinkedList do
   it "is an instance of list" do
     list = LinkedList.new
   
-
     expect(list).to be_an_instance_of(LinkedList)
     expect(list.head).to eq(nil)
   end
@@ -15,14 +14,20 @@ describe LinkedList do
     list = LinkedList.new
     list.append("doop")
 
-    # expect(list.head.data).to eq("doop")
   end
 
-  #new section here
   it "adds data to head" do
     list = LinkedList.new
     list.append("doop")
     
     expect(list.head.data).to eq("doop")
+  end
+
+  it "adds a next node" do
+    list = LinkedList.new
+    list.append("doop")
+
+    expect(list.head.next_node).to eq(nil)
+    require 'pry';binding.pry
   end
 end
