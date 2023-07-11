c6bf1eb4d605e027fd7509e64d70a560321b269d
Cory Powell <coryrpowell@gmail.com>
2023-07-10 21:21:36 -0400

    Add data to head

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -6,16 +6,23 @@ describe LinkedList do
   it "is an instance of list" do
     list = LinkedList.new
   
+
     expect(list).to be_an_instance_of(LinkedList)
     expect(list.head).to eq(nil)
-   
   end
   
   it "adds a new piece of data" do
     list = LinkedList.new
     list.append("doop")
 
-    require 'pry';binding.pry
+    # expect(list.head.data).to eq("doop")
   end
 
+  #new section here
+  it "adds data to head" do
+    list = LinkedList.new
+    list.append("doop")
+    
+    expect(list.head.data).to eq("doop")
+  end
 end
