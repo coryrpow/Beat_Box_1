c7486bdba5dc97b14bd7b14ab46d740a3c90049a
Cory Powell <coryrpowell@gmail.com>
2023-07-10 21:20:57 -0400

    Add head to LinkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,15 +1,18 @@
 class LinkedList
   attr_reader :head
 
-   def initialize
-    @head = nil
+  def initialize(head = nil)
+    @head
     
-
-
   end
 
   def append(data)
-    @data = ("doop")
-  end
+    # require 'pry';binding.pry
+    if @head.nil?
+    @head = Node.new(data)
+    end
 
+    # if head is equal to nil make head the node
+    # else find the tail of the linked list and add the node at end
+  end
 end
