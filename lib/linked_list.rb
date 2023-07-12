bd015dd6bb38e8b2f0655a0416d62132cbd021ed
Cory Powell <coryrpowell@gmail.com>
2023-07-11 20:11:52 -0400

    Add iteration 1 final

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,17 +1,34 @@
 class LinkedList
   attr_reader :head
 
-  def initialize(head = nil)
-    @head
+  def initialize()
+    @head = nil
     
   end
 
   def append(data)
-    # require 'pry';binding.pry
     if @head.nil?
-    @head = Node.new(data)
+      @head = Node.new(data)
+    else 
+      @head.next_node = Node.new(data)
     end
-    # if head is equal to nil make head the node
-    # else find the tail of the linked list and add the node at end
+  end
+
+  def count
+    counter = nil
+    if @head && @head.next_node.nil?
+      counter = 1
+    elsif @head && @head.next_node
+      counter = 2
+    end
+
+  end
+
+  def to_string
+    if count == 1
+      "#{@head.data}"
+    elsif count == 2
+      "#{@head.data} #{@head.next_node.data}"
+    end
   end
 end
